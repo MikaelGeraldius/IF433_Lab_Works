cbd774588b4dc4e7cac22f05fc83b9b8b1fa4a3f
gege <mikael.geraldius@student.umn.ac.id>
2026-02-08 22:58:47 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000113329_MikaelGeraldiusGunawan/week01/GameStore.kt
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week01/GameStore.kt
@@ -6,3 +6,12 @@ fun main() {
     val price: Int = 50000
 
 }
+
+fun calculateDiscoun(price: Int): Int {
+    if (price > 500000) {
+        return price - (price * 0.20).toInt()
+    }
+    else {
+        return price - (price * 0.10).toInt()
+    }
+}
