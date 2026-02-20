f63088ece6e328d8f45b245a66f1f613b2b12cd4
gege <mikael.geraldius@student.umn.ac.id>
2026-02-20 15:22:01 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000113329_MikaelGeraldiusGunawan/week03/Employee.kt
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_00000113329_MikaelGeraldiusGunawan.week03
 
 class Employee(var name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value
+        }
 }
