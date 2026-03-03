f3f2ed18b0fa725440534efa56d50e1192ef38a4
gege <mikael.geraldius@student.umn.ac.id>
2026-03-03 18:08:39 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week05/Main.kt
@@ -0,0 +1,28 @@
+package oop_00000113329_MikaelGeraldiusGunawan.week05
+
+import kotlin.io.path.Path
+
+fun main() {
+    val dosen1 = Dosen("Pak Alex", "0123456")
+    val admin1 = Admin("Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("--------------------------")
+    }
+}
