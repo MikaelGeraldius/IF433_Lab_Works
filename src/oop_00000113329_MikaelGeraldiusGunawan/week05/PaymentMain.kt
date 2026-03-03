9c2e7aaf6c439a9108c0771c69befcd889882bcd
gege <mikael.geraldius@student.umn.ac.id>
2026-03-03 18:32:51 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week05/PaymentMain.kt
@@ -0,0 +1,12 @@
+package oop_00000113329_MikaelGeraldiusGunawan.week05
+
+fun main() {
+    val ewallet1 = Ewallet(accountName = "GegeGopay", balance = 50000.0)
+    val credit1 = CreditCard(accountName = "GegeCredit", limit = 100000.0)
+
+    val daftarPaymentMethod : List<PaymentMethod> = listOf(ewallet1, credit1)
+
+    for (i in daftarPaymentMethod) {
+        i.processPayment(75000.0)
+    }
+}
