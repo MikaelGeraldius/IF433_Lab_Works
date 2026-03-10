776fb1b828ce211cc0d2471afff017a05c8649c0
gege <mikael.geraldius@student.umn.ac.id>
2026-03-10 15:42:59 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week06/MandiriMain.kt
@@ -0,0 +1,7 @@
+package oop_00000113329_MikaelGeraldiusGunawan.week06
+
+fun main() {
+    val lamp = SmartLamp("1","RuangTamu")
+    val speaker = SmartSpeaker("2", "Google Nest Dapur")
+    val cctv = SmartCCTV("3","Ezvix Garasi")
+}
