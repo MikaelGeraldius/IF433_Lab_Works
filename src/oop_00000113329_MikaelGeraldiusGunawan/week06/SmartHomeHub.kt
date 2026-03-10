65bbe100ed185f259a111728af245c19b277d307
gege <mikael.geraldius@student.umn.ac.id>
2026-03-10 15:38:38 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week06/SmartHomeHub.kt
@@ -0,0 +1,18 @@
+package oop_00000113329_MikaelGeraldiusGunawan.week06
+
+class SmartHomeHub () {
+    val device = mutableListOf<SmartDevice>()
+    fun addDevice(device: SmartDevice) {
+
+    }
+
+    fun turnOffAllSwitches() {
+        for (i in device) {
+            when (i) {
+                is Switchable -> {
+                    i.turnOff()
+                }
+            }
+        }
+    }
+}
