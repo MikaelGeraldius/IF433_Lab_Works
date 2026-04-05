38cd64842c613ab59123a204dcf94a87b986269a
gege <mikael.geraldius@student.umn.ac.id>
2026-04-05 21:48:13 +0700

    week07: (trial) trigger non-exhaustive when compiler error

---
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week07/Main3.kt
@@ -0,0 +1,11 @@
+package oop_00000113329_MikaelGeraldiusGunawan.week07
+
+fun main() {
+    println("\n=== TEST SEALED CLASS ===")
+    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
+
+    val uiMessage = when(response) {
+        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+    }
+}
