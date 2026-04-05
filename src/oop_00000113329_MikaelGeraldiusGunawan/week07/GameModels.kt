6253c4ba4f3085992eca92fb854af3199cb10ea3
gege <mikael.geraldius@student.umn.ac.id>
2026-04-05 22:05:27 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week07/GameModels.kt
@@ -0,0 +1,10 @@
+package oop_00000113329_MikaelGeraldiusGunawan.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(60),
+    UNCOMMON(25),
+    RARE(10),
+    EPIC(4),
+    LEGENDARY(1)
+}
+
