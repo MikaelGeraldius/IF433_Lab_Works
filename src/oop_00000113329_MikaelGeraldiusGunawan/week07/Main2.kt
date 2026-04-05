daa2705c61fc9e4c0a713e51560eddf264aa3588
gege <mikael.geraldius@student.umn.ac.id>
2026-04-05 21:33:35 +0700

    week07: (trial) demonstrate regular class equality failure

---
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week07/Main2.kt
@@ -0,0 +1,9 @@
+package oop_00000113329_MikaelGeraldiusGunawan.week07
+
+fun main() {
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Alice", 22)
+    val reg2 = RegularUser("Alice", 22)
+    println(reg1)
+    println("Sama? ${reg1 == reg2}")
+}
---
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week07/UserModels.kt
@@ -0,0 +1,3 @@
+package oop_00000113329_MikaelGeraldiusGunawan.week07
+
+class RegularUser(val name: String, val age: Int)
