ca1f6687fa1caa535970499195eaf49efcb7d077
gege <mikael.geraldius@student.umn.ac.id>
2026-04-05 22:38:58 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_00000113329_MikaelGeraldiusGunawan/week07/GameExecutor.kt
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week07/GameExecutor.kt
@@ -1,10 +1,10 @@
 package oop_00000113329_MikaelGeraldiusGunawan.week07
 
 fun processEvent(event: BattleState) {
-    val uiMessage = when (event) {
-        is BattleState.MonsterEncounter -> "Encounter ${event.monsterName}"
-        is BattleState.LootDropped -> "LootDropped Name = ${event.item.name}, Rarity = ${event.item.rarity}"
-        is BattleState.GameOver -> "GameOver because ${event.reason}"
-        is BattleState.SafeZone -> "Safe Zone"
+    when (event) {
+        is BattleState.MonsterEncounter -> println("Encounter ${event.monsterName}")
+        is BattleState.LootDropped -> println("LootDropped Name = ${event.item.name}, Damage = ${event.item.damage}, Rarity = ${event.item.rarity}")
+        is BattleState.GameOver -> println("GameOver because ${event.reason}")
+        is BattleState.SafeZone -> println("Safe Zone")
     }
 }
--- src/oop_00000113329_MikaelGeraldiusGunawan/week07/GameMain.kt
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week07/GameMain.kt
@@ -7,4 +7,11 @@ fun main() {
     println("${ItemRarity.LEGENDARY.dropChance}")
     val starter = Weapon.forgeStarterSword()
     println(starter.item)
+
+    val upgrade = starter.item.copy(damage = starter.item.damage + 25)
+
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(upgrade))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
