b57d452f86574949a725b8fdc12a52f60df1f869
gege <mikael.geraldius@student.umn.ac.id>
2026-04-14 15:30:17 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week08/TugasMandiri/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000113329_MikaelGeraldiusGunawan.week08.TugasMandiri;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId){
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
