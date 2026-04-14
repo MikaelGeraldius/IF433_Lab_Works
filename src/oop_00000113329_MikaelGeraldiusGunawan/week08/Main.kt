2c4769dd6c5cca411c8e878e7baebd87ff44204d
gege <mikael.geraldius@student.umn.ac.id>
2026-04-14 15:11:59 +0700

    week08: implement java interop using !!

--- src/oop_00000113329_MikaelGeraldiusGunawan/week08/Main.kt
+++ src/oop_00000113329_MikaelGeraldiusGunawan/week08/Main.kt
@@ -49,4 +49,10 @@ fun main() {
     } catch (e: NullPointerException) {
         println("CRASH (NPE)! Jangan gunkan !! secara sembarangan.")
     }
+
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 }
